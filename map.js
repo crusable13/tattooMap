aa85f47eb630566a109ff1570a481ab829043b75
Christopher <jcelitesmith@gmail.com>
2018-05-24 22:24:42 -0500

    Function for find the average location of shops
    We use the average location as the center of our map

--- map.js
+++ map.js
@@ -1,31 +1,53 @@
+function averageLocOfShop(shops) {
+    // adding all lats togher and all lon together
+    lat = 0
+    lon = 0
+    for (var i = 0; i < shops.length; i++) {
+        lat = lat + shops[i].location.lat;
+        lon = lon + shops[i].location.lon;
+    }
+    console.log("location " + lat + " " + lon);
+    // find the mean location or use KC
+    if (shops.length == 0) {
+        lat = 39.255
+        lon = -94.361
+    } else {
+        lat = lat / shops.length
+        lon = lon / shops.length
+    }
+    console.log("location " + lat + " " + lon);
+    return { lat: lat, lng: lon };
+}
+
 
 function initMap() {
-  var myLatLng = {lat: 39.255, lng: -94.361};
+    shops = getShops();
 
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 9,
-    center: myLatLng
-  });
+    centerLoc = averageLocOfShop(shops);
 
-  shops = getShops();
-  for(i = 0; i < shops.length; i++) {
-    var shop = shops[i];
-    new google.maps.Marker({
-      position: {lat: shop.location.lat, lng: shop.location.lon},
-      map:map,
-      title: shop.shopName,
-      clickable:true,
-      label: shop.shopName
-      /// Label allows for name to be seen at all times for every location. Find a way to have it show you less of them as you zoom out.
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 9,
+        center: centerLoc
     });
 
 
-  }
+    for (i = 0; i < shops.length; i++) {
+        var shop = shops[i];
+        new google.maps.Marker({
+            position: { lat: shop.location.lat, lng: shop.location.lon },
+            map: map,
+            title: shop.shopName,
+            clickable: true,
+            label: shop.shopName
+            /// Label allows for name to be seen at all times for every location. Find a way to have it show you less of them as you zoom out.
+        });
 
-  var marker = new google.maps.Marker({
-    position: myLatLng,
-    map:map,
-    title: 'Can you see'
-  });
+
+    }
+
+    var marker = new google.maps.Marker({
+        position: centerLoc,
+        map: map,
+        title: 'Can you see'
+    });
 }
-
--- shop.js
+++ shop.js
@@ -1,42 +1,31 @@
-
 function loc(lat, lon) {
-  this.lat = lat;
-  this.lon = lon;
+    this.lat = lat;
+    this.lon = lon;
 }
 
 function shop(location, name, url) {
-  this.location = location;
-  this.shopName = name;
-  this.url = url;
+    this.location = location;
+    this.shopName = name;
+    this.url = url;
 }
 
 function getShops() {
-
-  return [  
-  new shop(new loc(39.255, -94.361), "Exile Tattoo", "exiletattoo.net"),    
-  new shop(new loc(39.340, -94.345), "The Mercy Seat", "http://www.mercyseattattoo.com"),
-  new shop(new loc(38.593, -94.354), "Irezumi Tattoo", "http://irezumitattookc.com/"),
-  new shop(new loc(39.354, -94.357), "Ink Parlor", 
-  "http://www.theinkparlor.com"),
-  new shop(new loc(38.564, -94.350),"Beautiful Body Tattoo","http://www.beautifulbodytattoo.com"),
-
-  new shop(new loc(39.136, -94.343), "A1 Tattoo Co",
-  "https://www.facebook.com/A1-Tattoo-Co-1384699290-94/"),
-
-  new shop(new loc(39.519, -94.352), "Old Souls tattoo Parlor",
-  "http://oldsoulstattoo.com/home.html"),
-
-  new shop(new loc(39.258, -94.356), "Stormy Tattoo", "www.stormytattoo.com"),
-
-  new shop(new loc(39.531, -94.358), "Windhorse Tattoo and Gallery",
-    
-  "www.NOWEBSITE.ORG"),
-
-  new shop(new loc(39.320, -94.354), "Hand and Dagger Tattoo", 
-  "https://www.handanddaggertattoo.com")
-  ]
+    return [
+        new shop(new loc(39.255, -94.361), "Exile Tattoo", "exiletattoo.net"),
+        new shop(new loc(39.340, -94.345), "The Mercy Seat", "http://www.mercyseattattoo.com"),
+        new shop(new loc(38.593, -94.354), "Irezumi Tattoo", "http://irezumitattookc.com/"),
+        new shop(new loc(39.354, -94.357), "Ink Parlor",
+            "http://www.theinkparlor.com"),
+        new shop(new loc(38.564, -94.350), "Beautiful Body Tattoo", "http://www.beautifulbodytattoo.com"),
+        new shop(new loc(39.136, -94.343), "A1 Tattoo Co",
+            "https://www.facebook.com/A1-Tattoo-Co-1384699290-94/"),
+        new shop(new loc(39.519, -94.352), "Old Souls tattoo Parlor",
+            "http://oldsoulstattoo.com/home.html"),
+        new shop(new loc(39.258, -94.356), "Stormy Tattoo", "www.stormytattoo.com"),
+        new shop(new loc(39.531, -94.358), "Windhorse Tattoo and Gallery",
+            "www.NOWEBSITE.ORG"),
+        new shop(new loc(39.320, -94.354), "Hand and Dagger Tattoo",
+            "https://www.handanddaggertattoo.com")
+    ]
 
 }
-
-
-
