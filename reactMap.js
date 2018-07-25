c2d87c5a858158bf8c296fa5cc3c9761073a512a
Christopher <jcelitesmith@gmail.com>
2018-07-25 09:27:27 -0500

    reactMap.js before attempting to add markers

--- reactMap.js
+++ reactMap.js
@@ -5,108 +5,91 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-var shops = getShops();
-var centerLoc = averageLocOfShops(shops);
-
-
-
-class initMap2() {
-    /*var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 9,
-        center: centerLoc
-    });
-    var markers = [];
-	*/
-	// Everything above is what I tried to implement in the constructor. 
-	// Please let me know I am misusing the constructor, or if I am not implementing this correctly. Thank you. Sorry I didn't get much done before the BBQ
-	constructor(props) {
-		this.map = {new google.maps.Map(document.getElementById('map'), {
-			zoom: 9,
-			center: centerLoc
-		})};
-
-		this.markers = {[]};
-
-	} 
-
-    for (i = 0; i < shops.length; i++) {
-        var shop = shops[i];
-
-        marker = new google.maps.Marker({
-            position: { lat: shop.location.lat, lng: shop.location.lon },
-            map: map,
-            title: shop.shopName,
-            clickable: true,
-            label: shop.shopName,
-            zoom: 5
-            /// Label allows for name to be seen at all times for every location. Find a way to have it show you less of them as you zoom out
-        });
-
-        marker.setClickable(true);
-        shop.callOutName();
-        console.log("shop url " + shop.url);
-
-        urlShop(shop, marker);
-         markers.push(marker);
-    }
-
-    map.addListener('zoom_changed', function() {
-        if (map.getZoom() < 9) {
-            for(i = 0; i < markers.length; i++) {
-                markers[i].setLabel(undefined);
-            }
-        } else {
-            for(i = 0; i < markers.length; i++) {
-                markers[i].setLabel(shops[i].shopName)
-            }
-        }
-        console.log("zoom_changed event fired on map:" + map.getZoom() )
-    });
-
-
-    var marker = new google.maps.Marker({
-        position: centerLoc,
-        map: map,
-        label: 'Home'
-    })
-
+var returnedValues = ["div", "h1", "h2"];
+var jsx = ["h1", "h2", "div", "App", "SearchBox", "form", "input"];
+var varl = ["h1", "h2", "div", "form", "input"]; // https://developer.mozilla.org/en-US/docs/Web/HTML/Element
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+        <SearchBox name="Nathan" showName={true} />
+        <h1>Another header</h1>
+        <SearchBox name="Chris" showName={true} />
+        <h1> Mom's SearchBox </h1>
+        <SearchBox name="Fran" showName={true} />
+      </div>
+    );
+  }
 }
 
-
-
-
-function averageLocOfShops(shops) {
-	lat = 0
-	lon = 0
-	for (var i = 0; i < shops.length; i++) {
-        lat = lat + shops[i].location.lat;
-        lon = lon + shops[i].location.lon;
-    }
-    console.log("location " + lat + " " + lon);
-    // find the mean location or use KC
-    if (shops.length == 0) {
-        lat = 39.255
-        lon = -94.361
-    } else {
-        lat = lat / shops.length
-        lon = lon / shops.length
-    }
-    console.log("location " + lat + " " + lon);
-    return { lat: lat, lng: lon };
+class SearchBox extends React.Component {
+  render() {
+    return (
+      <form>
+        {this.props.showName === true ? this.props.name : "Nothing"}
+        <input type="text" />
+      </form>
+    );
+  }
 }
 
-function urlShop(aShop, aMarker) {
-    aMarker.addListener('click', function(clickEvent) {
-        window.location.href = aShop.url
-    })
-}
-
-function visiMarker(aMarker) {
-    // Replaced google.maps.event with aMarker
-    aMarker.event.addListener(map, 'zoom_changed', function() {
-        var zoom = marker[i].getZoom();
-        for (i = 0; i < locations.length; i++) {
-            marker[i].setVisible(zoom <= 15);
-        }
+class MapComponent extends React.Component {
+  componentDidMount() {
+    var google_script_tag = document.getElementById("google_api");
+    console.log("google script tag=" + google_script_tag);
+    
+    if (google_script_tag === null) {
+      window.displayMap = this.displayMap;
+      const script = document.createElement("script");
+      script.defer = true;
+      script.id = "google_api";
+      script.src =
+        "https://maps.googleapis.com/maps/api/js?key=AIzaSyAPL1Ofx-vSERWL0kk3JYTtugha2sgv_Xs&callback=displayMap";
+      script.async = true;
+      document.body.appendChild(script);
+    }
+    console.log("Did mount");
+  }
+  displayMap() {
+    console.log("this is a:" + this);
+    console.log("displayMap called");
+    var map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 9,
+      center: { lat: 39.255, lng: -94.361 }
     });
-};
+    if (this.props.shops != null) {
+      console.log("about to add shop markers");
+      /// Add markers here///
+
+      
+    }
+  }
+
+  constructor(props) {
+    super(props);
+    this.displayMap = this.displayMap.bind(this);
+    // intentionally empty
+  }
+  render() {
+    const style = {
+      width: "100vw",
+      height: "100vh",
+      backgroundColor: "blue"
+    };
+    return (
+      <div>
+        <div style={style} id="map">
+          Map here
+        </div>
+        <div />
+      </div>
+    );
+  }
+}
+var shops = [{ lat: 39.255, lng: -94.361 }, { lat: 39.5, lng: -94.361 }];
+
+const rootElement = document.getElementById("root");
+
+ReactDOM.render(<MapComponent shops={shops} />, rootElement);
