4f668c2c4625a08be825ef3bdee52b278b704a14
Christopher <jcelitesmith@gmail.com>
2018-09-18 10:34:12 -0500

    Map import fixed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./App.css";
 import locations from "./shop.js";
-import MapComponent from "./reactMap.js"
+import {MapComponent} from "./reactMap.js"
 
 function myBind(aFunction, obj) {
   return function () {
