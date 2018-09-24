50f2970d613875e167eb14a8cef230c81e5efd3d
Christopher <jcelitesmith@gmail.com>
2018-09-24 08:26:22 -0500

    Testing new id prefix code

--- src/ResultListComponent.js
+++ src/ResultListComponent.js
@@ -5,10 +5,11 @@ import ReactDOM from "react-dom";
 export class ResultCellComponent extends React.Component {
   componentDidMount() {
     console.log("componentDidMount Called " + this.props.shop.id)
-    var rowElement = document.getElementById(this.props.shop.id)
+
+    var rowElement = document.getElementById(this.props.idPrefix + this.props.shop.id)
     var innerResultHover = this.props.resultHover;
     var shop = this.props.shop;
-    rowElement.addEventListener('mouseover', function () {
+    rowElement.addEventListener('mouseover', function() {
       innerResultHover(shop.id)
     })
   };
@@ -24,7 +25,7 @@ export class ResultCellComponent extends React.Component {
       border: "2px solid black"
     };
     return (
-      <tr id={this.props.shop.id}>
+      <tr id={this.props.idPrefix + this.props.shop.id}>
         <td style={style}>
           {this.props.shop.shopName}
         </td>
@@ -34,33 +35,55 @@ export class ResultCellComponent extends React.Component {
 };
 const shops = [
   {
-    id: 1,
+    id: 0,
     shopName: "My shop",
     location: { lat: 134, lng: 200 }
   },
   {
-    id: 2,
+    id: 1,
     shopName: "Tat shop",
     location: { lat: 155, lng: 200 }
   }
 ]
 const rootElement = document.getElementById("root")
-
-function onResultHover(id) {
+ onResultHover(id) {
   console.log("onResultHover called " + id);
+  this.setState({ hightlightIndex: id });
 };
 
 
 export class ResultListComponent extends React.Component {
   constructor(props) {
     super(props)
+    this.state = { hightlightIndex: -1 };
   }
+
+  onResultHover(id) {
+  console.log("onResultHover called " + id);
+  this.setState({ hightlightIndex: id });
+};
+
   render() {
     var cells = [];
+    var color = "red";
     for (var i = 0; i < this.props.shops.length; i++) {
-      var cell = <ResultCellComponent key={shops[i].id} shop={shops[i]} resultHover={onResultHover} />
-      cells.push(cell)
+      if (this.state.hightlightIndex === i) {
+        color = "green";
+      } else {
+        color = "red";
+      }
+      var cell = (
+        <ResultCellComponent
+          idPrefix="first"
+          key={shops[i].id}
+          shop={shops[i]}
+          resultHover={this.onResultHover.bind(this)}
+          color={color}
+        />
+      );
+      cells1.push(cell);
     }
+    
     return (
       /*
       <style>
